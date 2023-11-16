e699c52f85f85e5266566af83948a313643382e0
Mattn4 <nky_88@hotmail.com>
2023-11-16 10:24:19 +0800

    improved codes for all queries

--- assessment5_nky.sql
+++ assessment5_nky.sql
@@ -1,47 +1,66 @@
 -- What are the Top 25 schools (.edu domains)?
-SELECT COUNT(user_id), email_domain FROM users
+SELECT COUNT() AS "Number of students", email_domain 
+FROM users
 GROUP BY email_domain
-ORDER BY COUNT(user_id) DESC
+ORDER BY COUNT() DESC
 LIMIT 25;
 
 
--- How many .edu learners are located in New York? Answer: 50 if exclude West New York
-SELECT COUNT(user_id), city FROM users 
+-- How many .edu learners are located in New York? Answer: 50 
+-- Note: West New York is not part of New York
+SELECT COUNT() AS "Number of students", city 
+FROM users 
 WHERE city = "New York";
 
 
 -- The mobile_app column contains either mobile-user or NULL. 
 -- How many of these Codecademy learners are using the mobile app? Answer: 330
-SELECT COUNT(user_id), mobile_app FROM users 
+SELECT COUNT() AS "Number of students", mobile_app 
+FROM users 
 WHERE mobile_app != "";
--- WHERE mobile_app = "mobile-user"
+--  or can write as: WHERE mobile_app = "mobile-user"
 
 
 -- query for the sign up counts for each hour
-SELECT COUNT(user_id),
-   strftime('%H', sign_up_at)
+SELECT COUNT() AS "Number of students",
+                strftime('%H', sign_up_at) AS Hour
 FROM users
-GROUP BY strftime('%H', sign_up_at)
-ORDER BY strftime('%H', sign_up_at) ASC;
+GROUP BY Hour
+ORDER BY Hour ASC;
 
 
 -- Do different schools (.edu domains) prefer different courses?
-SELECT progress.*, users.email_domain
+SELECT users.email_domain, 
+COUNT() FILTER (WHERE learn_cpp != "") AS "Learn C++",
+COUNT() FILTER (WHERE learn_sql != "") AS "learn SQL",
+COUNT() FILTER (WHERE learn_html != "") AS "Learn HTML",
+COUNT() FILTER (WHERE learn_javascript != "") AS "Learn JavaScript",
+COUNT() FILTER (WHERE learn_java != "") AS "Learn Java"
 FROM progress
 INNER JOIN users ON progress.user_id = users.user_id
-ORDER BY users.email_domain ASC;
+GROUP BY users.email_domain
+ORDER BY users.email_domain ASC
+LIMIT 50;
+
+-- This query is to check if count for the query above is correct or not
+-- SELECT users.email_domain, progress.* 
+-- FROM progress
+-- INNER JOIN users ON progress.user_id = users.user_id
+-- ORDER BY users.email_domain ASC
+-- LIMIT 100;
 
 
--- What courses are the New Yorkers students taking? (excludes West New York)
+-- What courses are the New Yorkers students taking?
+-- Note: West New York is excluded as it is not part of New York 
 SELECT progress.*, users.city
 FROM progress
 INNER JOIN users ON progress.user_id = users.user_id
 WHERE users.city = "New York";
 
 
--- What courses are the Chicago students taking? (excludes West Chicago)
+-- What courses are the Chicago students taking? (includes West Chicago)
 SELECT progress.*, users.city
 FROM progress
 INNER JOIN users ON progress.user_id = users.user_id
-WHERE users.city = "Chicago";
+WHERE users.city LIKE "%Chicago%";
 
